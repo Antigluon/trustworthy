8a982da7c01d648c79c11ee282f83f277c838eb0
Eric Andrechek <ericandrechek@Erics-MacBook-Pro.local>
2020-05-30 15:40:16 -0400

    added flask api code

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, jsonify
+import json
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/api', methods = ['GET', 'POST'])
+@cross_origin()
+def article_check():
+    if request.method == 'POST':
+        url = request.form.get('article_url')
+        if url is not None:
+            # do ml stuff
+            return "all good"
+        else:
+            return "didn't get an article url"
+    else:
+        return redirect('https://google.com') # this will be the url of our pretty home page 
+        # hopefully? or maybe redirect to extension
+
+@app.route('/')
+@cross_origin()
+def home():
+    # either gonna display a home page where you can upload an article URL yourself, or take you to the chrome extension
+    return "lets win this thing"
+
+if __name__ == '__main__':
+    app.run(debug = True, threaded = True)
